20a3798933ba278831d787397514cc044aa5c31e
Tobias Feldballe <ext_tfpe@mir-robots.com>
2024-12-11 14:41:04 +0100

    Added license

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"golang.org/x/exp/rand"
 )
 
 func main() {
@@ -27,41 +28,79 @@ func main() {
 		log.Panic("Error connecting to MQTT broker:", token.Error())
 	}
 	nodeStates := []models.NodeState{}
+
+	defaultMap := models.Map{
+		MapID:          "99187cd1-8b4b-4f5a-ac11-e455928409de",
+		MapVersion:     "0.1.1",
+		MapStatus:      "beta",
+		MapDescription: "Just a random map",
+	}
+
 	state := models.State{
-		HeaderID:              0,
-		Timestamp:             "2024-11-18T11:40:03.12Z",
-		Version:               "1.2.3",
-		Manufacturer:          "BananaRepublic",
-		SerialNumber:          "d9c717ca-158d-4a63-ac2b-45aa8973a5f3",
-		OrderID:               "",
-		OrderUpdateID:         0,
-		LastNodeID:            "",
-		LastNodeSequenceID:    0,
-		NodeStates:            nodeStates,
-		EdgeStates:            []models.EdgeState{},
-		Driving:               false,
-		ActionStates:          []models.ActionState{},
-		BatteryState:          models.BatteryState{},
+		HeaderID:           0,
+		Timestamp:          "2024-11-18T11:40:03.12Z",
+		Version:            "1.2.3",
+		Manufacturer:       "Banana Republic",
+		SerialNumber:       "d9c717cb-158d-4a63-ac2b-45aa8973a5f3",
+		OrderID:            "",
+		OrderUpdateID:      0,
+		LastNodeID:         "",
+		LastNodeSequenceID: 0,
+		NodeStates:         nodeStates,
+		EdgeStates:         []models.EdgeState{},
+		Driving:            false,
+		ActionStates:       []models.ActionState{},
+		BatteryState: models.BatteryState{
+			BatteryCharge:  99,
+			Charging:       false,
+			BatteryVoltage: 14,
+			BatteryHealth:  100,
+			Reach:          0,
+		},
 		OperatingMode:         "",
 		Errors:                []models.Error{},
 		SafetyState:           models.SafetyState{},
-		Maps:                  []models.Map{},
+		Maps:                  []models.Map{defaultMap},
 		ZoneSetID:             "",
 		Paused:                false,
 		NewBaseRequest:        false,
 		DistanceSinceLastNode: 0,
-		AgvPosition:           models.AgvPosition{},
-		Velocity:              models.Velocity{},
-		Loads:                 []models.Load{},
-		Information:           []models.Info{},
+		AgvPosition: models.AgvPosition{
+			X:                   150,
+			Y:                   150,
+			Theta:               0,
+			MapID:               defaultMap.MapID,
+			PositionInitialized: false,
+			MapDescription:      defaultMap.MapDescription,
+			LocalizationScore:   0,
+			DeviationRange:      0,
+		},
+		Velocity: models.Velocity{
+			Vx:    10,
+			Vy:    10,
+			Omega: 0,
+		},
+		Loads:       []models.Load{},
+		Information: []models.Info{},
 	}
-	message, err := json.Marshal(state)
-	if err != nil {
-		log.Fatal(err)
-	}
+
 	for {
+		xDirection := (rand.Float64() - 0.5) * 20
+		yDirection := (rand.Float64() - 0.5) * 20
+		state.BatteryState.BatteryCharge--
+		if state.BatteryState.BatteryCharge < 30 {
+			state.BatteryState.BatteryCharge = 99
+		}
+
+		state.AgvPosition.X = state.AgvPosition.X - xDirection
+		state.AgvPosition.Y = state.AgvPosition.Y - yDirection
+
+		message, err := json.Marshal(state)
+		if err != nil {
+			log.Fatal(err)
+		}
 		token := client.Publish("state", 0, false, message)
 		token.Wait()
-		time.Sleep(15 * time.Second)
+		time.Sleep(5 * time.Second)
 	}
 }
--- models/state.go
+++ models/state.go
@@ -51,16 +51,6 @@ type Error struct {
 	ErrorHint        string        `json:"errorHint"`
 }
 
-type AgvPosition struct {
-	X                   float64 `json:"x"`
-	Y                   float64 `json:"y"`
-	Theta               float64 `json:"theta"`
-	MapID               string  `json:"mapId"`
-	PositionInitialized bool    `json:"positionInitialized"`
-	MapDescription      string  `json:"mapDescription"`
-	LocalizationScore   float64 `json:"localizationScore"`
-	DeviationRange      float64 `json:"deviationRange"`
-}
 type Map struct {
 	MapID          string `json:"mapId"`
 	MapVersion     string `json:"mapVersion"`
@@ -70,11 +60,6 @@ type Map struct {
 type SafetyState struct {
 	EStop          string `json:"eStop"`
 	FieldViolation bool   `json:"fieldViolation"`
-}
-type Velocity struct {
-	Vx    float64 `json:"vx"`
-	Vy    float64 `json:"vy"`
-	Omega float64 `json:"omega"`
 }
 
 type Info struct {
